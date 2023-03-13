a1fd805b756a7f8b47f50e71c98a092cdfeec331
cryptosvinarnik <na4030680@gmail.com>
2023-03-13 21:33:02 +0200

    proxy support

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import asyncio
 
+from loguru import logger
+
 from consensys import worker
 
 
 async def main():
     q = asyncio.Queue()
 
-    for email in open(input("File with emails >> ")).readlines():
-        await q.put(email.strip())
+    with open("emails.txt") as f:
+        accounts = f.read().splitlines()
+
+    for account in accounts:
+        if len(account.split("|")) == 2:
+            q.put_nowait(account.split("|"))
+        elif len(account.split("|")) == 1:
+            q.put_nowait([account, None])
+        else:
+            logger.warning(f"Invalid account: {account}, skipping...")
+
 
     await asyncio.gather(
         *[worker(i, q) for i in range(1, 10)]
@@ -19,12 +30,3 @@ if __name__ == "__main__":
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
         pass
-
-# create README.md for this project in the next commentary
-# For install this script you need to install python3 and pip3
-# Install the requirements:
-# pip3 install -r requirements.txt
-# Please fill the CAPMONSTER_API_KEY in the config.py file and write your emails in the emails.txt file
-# Run the script:
-# python3 main.py
-
